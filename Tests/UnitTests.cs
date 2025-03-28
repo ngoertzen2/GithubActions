ef75710b5eaa6819de44360fbfb9dc28e7ce47c7
Nick Goertzen <ngoertzen2@huskers.unl.edu>
2025-03-28 16:33:57 -0500

    Unit Tests

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -9,6 +9,8 @@ public class Addition
 		Assert.AreEqual(3, Program.Add("1", "2"));
 		Assert.AreEqual(5, Program.Add("3", "2"));
 		Assert.AreEqual(12, Program.Add("5", "7"));
+		Assert.AreEqual(1, Program.Subtract("2", "1"));
+
 	}
 
 	[TestMethod]
@@ -25,5 +27,34 @@ public class Addition
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Add("1", null));
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, "1"));
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, null));
+	}
+}
+
+[TestClass]
+public class Subtraction
+{
+	[TestMethod]
+	public void Add_Valid_Patino()
+	{
+		Assert.AreEqual(3, Program.Subtract("1", "2"));
+		Assert.AreEqual(5, Program.Subtract("3", "2"));
+		Assert.AreEqual(12, Program.Subtract("5", "7"));
+
+	}
+
+	[TestMethod]
+	public void Add_Invalid_Patino()
+	{
+		Assert.ThrowsException<FormatException>(() => Program.Subtract("1", "a"));
+		Assert.ThrowsException<FormatException>(() => Program.Subtract("a", "1"));
+		Assert.ThrowsException<FormatException>(() => Program.Subtract("a", "a"));
+	}
+
+	[TestMethod]
+	public void Add_Null_Patino()
+	{
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract("1", null));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract(null, "1"));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract(null, null));
 	}
 }
